8b67f931d68ce844ca00bce3296704de284be2f7
samin <samin@DESKTOP-QSVDD4R.local>
2019-06-05 10:21:56 +0530

    This is my first git repo commit

---
+++ MytestGit/src/main/java/com/saminda/Test1.java
@@ -0,0 +1,11 @@
+package com.saminda;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is test page");
+
+	}
+
+}
