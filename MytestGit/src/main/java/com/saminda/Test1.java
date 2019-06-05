ec5b40d7fea7035c834dbdcec6abaf3762e6a8a4
samin <samin@DESKTOP-QSVDD4R.local>
2019-06-05 10:59:20 +0530

    this is my second

--- MytestGit/src/main/java/com/saminda/Test1.java
+++ MytestGit/src/main/java/com/saminda/Test1.java
@@ -5,7 +5,9 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is test page");
-
+		
+		System.out.println("This is my second page");
+		
 	}
 
 }
